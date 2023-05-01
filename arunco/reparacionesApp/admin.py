ef1f1921a45ccd285f019461b016b1ad30f28755
Francisco Medina <medinafran2002@hotmail.com>
2023-05-01 11:49:42 -0300

    feat: add models to admin

--- arunco/reparacionesApp/admin.py
+++ arunco/reparacionesApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Reparaciones)
+admin.site.register(Clientes)
+admin.site.register(Trabajos)
+admin.site.register(ManoDeObra)
+admin.site.register(Repuestos)
+admin.site.register(Encargos)
